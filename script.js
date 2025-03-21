aaac6355891db416601bf720e436857eeb6e745e
Micahtech <157807742+Micahtech@users.noreply.github.com>
2025-03-21 10:57:43 -0700

    port

--- script.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector(".burger")
-const menuItems = document.querySelector(".menu-items")
-
-burger.addEventListener("click", () => {
-    // menuItems.document.style.display = "block"
-    console.log("Clicked")
-    menuItems.classList.toggle("active")
-})
